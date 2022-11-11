55e5700e9c833255b3fce88fd4587cf20134289b
AmitUdugade <amit.udugade18@gmail.com>
2022-11-11 15:41:11 +0530

    Changes: This is day one of class. Created clas that define properties and methods and display the Student data. Changed By: Amit Udugade Changed Date: 11/10/2022

---
+++ src/ClassEmployeeDay1.java
@@ -0,0 +1,7 @@
+public class ClassEmployeeDay1 {
+    public static void main(String[] args) {
+        System.out.println("Employee Details");
+
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+    int rollno;
+    String name;
+    int standard;
+    int marks;
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStandard() {
+        return standard;
+    }
+
+    public void setStandard(int standard) {
+        this.standard = standard;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
